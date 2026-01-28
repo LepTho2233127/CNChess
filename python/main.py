420dd30b0e4b479f505c873c9ac7e46aa43f1eb2
LepTho2233127 <thomasll05@icloud.com>
2026-01-28 10:46:25 -0500

    clean up of ui

--- python/main.py
+++ python/main.py
@@ -34,29 +34,3 @@ if __name__ == "__main__":
     
     # Run the application event loop
     sys.exit(app.exec())
-
-    while not game.check_game_over():
-        # Here you would normally get the player's move from input
-        path = []
-        if game.get_turn()== game.get_player_color():
-            player_move = game.get_next_best_move()  # Placeholder for actual player move
-            if game.validate_move(player_move):
-                game.set_player_move(player_move)
-                control.update_board_state(game.get_board_state())
-                game.make_move(game.get_player_move())
-            else: 
-                print("Invalid move by player.")
-        else:
-            game.set_computer_move(game.get_next_best_move())
-            game.make_move(game.get_computer_move())
-            control.update_board_state(game.get_board_state())
-            path = control.get_path(game.get_computer_move())
-            control.print_path(path)
-            traj = control.calculate_trajectory(path)
-            control.print_trajectory(traj)
-        
-        game.display_board()
-
-    game.display_board()
-    print("Game over!")
-    wait = input("Press Enter to exit...")
