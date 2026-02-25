a93bbcbc375f12b2f9dd03c481d945b564de7d58
Zach3292 <zach3292@hotmail.com>
2026-02-25 14:48:04 -0500

    add impossible difficuly

--- python/CNChess.py
+++ python/CNChess.py
@@ -21,11 +21,13 @@ class CNChess:
         self.next_player_move = None
         self.computer = stockfish.Stockfish(path=self.stockfish_path, depth=self.stockfish_depth)
         self.EASY_ELO = 1350
-        self.EASY_DEPTH = 5
+        self.EASY_DEPTH = 1
         self.MEDIUM_ELO = 1350
-        self.MEDIUM_DEPTH = 10
-        self.HARD_ELO = 1700
+        self.MEDIUM_DEPTH = 5
+        self.HARD_ELO = 2000
         self.HARD_DEPTH = 10
+        self.IMPOSSIBLE_ELO = 3000
+        self.IMPOSSIBLE_DEPTH = 20
 
 
     def set_elo(self, elo: int):
@@ -37,11 +39,11 @@ class CNChess:
     def set_difficulty(self, difficulty: str):
         """Set the difficulty level of the computer opponent. Valid values are 'easy', 'medium', and 'hard'."""
 
-        if difficulty not in ["easy", "medium", "hard"]:
-            raise ValueError("Invalid difficulty level. Must be 'easy', 'medium', or 'hard'.")
+        if difficulty not in ["easy", "medium", "hard", "impossible"]:
+            raise ValueError("Invalid difficulty level. Must be 'easy', 'medium', 'hard', or 'impossible'.")
 
-        elo = self.EASY_ELO if difficulty == "easy" else self.MEDIUM_ELO if difficulty == "medium" else self.HARD_ELO
-        depth = self.EASY_DEPTH if difficulty == "easy" else self.MEDIUM_DEPTH if difficulty == "medium" else self.HARD_DEPTH    
+        elo = self.EASY_ELO if difficulty == "easy" else self.MEDIUM_ELO if difficulty == "medium" else self.HARD_ELO if difficulty == "hard" else self.IMPOSSIBLE_ELO
+        depth = self.EASY_DEPTH if difficulty == "easy" else self.MEDIUM_DEPTH if difficulty == "medium" else self.HARD_DEPTH if difficulty == "hard" else self.IMPOSSIBLE_DEPTH
 
         self.set_elo(elo)
         self.set_depth(depth)
